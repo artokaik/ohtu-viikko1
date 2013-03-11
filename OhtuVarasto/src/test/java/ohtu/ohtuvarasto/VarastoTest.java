30580b3da48c0b731d4905e2063378738ff0e5db
Arto Kaikkonen <kaikkonen.arto@gmail.com>
2013-03-11 22:02:24 +0200

    JUnit

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -69,10 +69,95 @@ public class VarastoTest {
     public void konstr() {
         varasto = new Varasto(-1);
         varasto = new Varasto(0);
-        varasto = new Varasto(1,1);
-        varasto = new Varasto(1,2);
-        varasto = new Varasto(-1,2);
-        varasto = new Varasto(-1,-1);
+        varasto = new Varasto(1, 1);
+        varasto = new Varasto(1, 2);
+        varasto = new Varasto(-1, 2);
+        varasto = new Varasto(-1, -1);
         varasto.toString();
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonAlleNolla() {
+        varasto.lisaaVarastoon(-1.0);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonNolla() {
+        varasto.lisaaVarastoon(0);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonKaksiJaKolme() {
+        varasto.lisaaVarastoon(2);
+        varasto.lisaaVarastoon(3);
+        assertEquals(5, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonViisiJaViisi() {
+        varasto.lisaaVarastoon(5);
+        varasto.lisaaVarastoon(5);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonKuusiJaViisi() {
+        varasto.lisaaVarastoon(6);
+        varasto.lisaaVarastoon(5);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonKuusiJaOtaAlleNollaPalauttaaNolla() {
+        varasto.lisaaVarastoon(6);
+        assertEquals(0, varasto.otaVarastosta(-2), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonKuusiJaOtaAlleNollaEiMuutaVarastosaldoa() {
+        varasto.lisaaVarastoon(6);
+        varasto.otaVarastosta(-2);
+        assertEquals(6, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonNeljaJaOtaNollaPalauttaaNolla() {
+        varasto.lisaaVarastoon(4);
+        assertEquals(0, varasto.otaVarastosta(0), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonViisiJaOtaNollaEiMuutaSaldoa() {
+        varasto.lisaaVarastoon(5);
+        varasto.otaVarastosta(0);
+        assertEquals(5, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonKuusiJaOtaViisiPalauttaaViisi() {
+        varasto.lisaaVarastoon(6);
+        assertEquals(5, varasto.otaVarastosta(5), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonKuusiJaOtaViisiJaSaldoOnYksi() {
+        varasto.lisaaVarastoon(6);
+        varasto.otaVarastosta(5);
+        assertEquals(1, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+        @Test
+    public void lisaaTyhjaanVarastoonKolmeJaOtaViisiPalauttaaKolme() {
+        varasto.lisaaVarastoon(3);
+        assertEquals(3, varasto.otaVarastosta(5), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaTyhjaanVarastoonKolmeJaOtaViisiJaSaldoOnNolla() {
+        varasto.lisaaVarastoon(3);
+        varasto.otaVarastosta(5);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
 }
